9449e22c0815754988919938f91404329f34386d
root <adubus@intuitiv.fr>
2016-11-01 14:10:30 +0000

    update of customisations scripts

--- custom_shell.sh
+++ custom_shell.sh
@@ -1,31 +1,19 @@
 #!/bin/bash
-apt-get install vim
-    if [[ ! -f /etc/vim/vimrc.local ]]; then
-        echo -e "syntax on \
-            \nset smarttab \
-            \nset noet ci pi sts=0 sw=4 ts=4 \
-            \nset cursorline \
-            \nfiletype plugin indent on \
-            \nset t_Co=256 \
-            \nset background=dark \
-            \nset titlestring=%f title \
-            \nset nobk nowb noswf \
-            \nset tabstop=2 \
-            \nset shiftwidth=2 \
-            \nset expandtab \
-			\nset mouse=a \
-			\nset incsearch
-            " > /etc/vim/vimrc.local
-        #log "Configuration of Vim in /etc/vim/vimrc.local"
-    fi
 
-#Color root Shell
-cat >> /root/.bashrc << _EOF_
-export LS_OPTIONS='--color=auto'
-eval "`dircolors`"
-alias ls='ls $LS_OPTIONS'
-_EOF_
 
-. /root/.bashrc
+# Custom bashrc
+cp /root/.bashrc /root/.bashrc.orig
+sed -i 's/^# export LS_OPTIONS=/export LS_OPTIONS=/g' /root/.bashrc
+sed -i 's/^# eval "`dircolors`"/eval "`dircolors`"/g' /root/.bashrc
+sed -i 's/^# alias ls=/alias ls=/g' /root/.bashrc
+sed -i 's/^# alias ll=/alias ll=/g' /root/.bashrc
+sed -i 's/^# alias l=/alias l=/g' /root/.bashrc
+
+echo "
+alias al=\"ls \$LS_OPTIONS -alh\" 
+alias showconnections=\"netstat -ntu | awk '{print \$5}' | cut -d: -f1 | grep -E [0-9.]+ | sort | uniq -c | sort -n\" 
+" >> /root/.bashrc
+
 echo "Think to do a '. /root/.bashrc' if the modif aren't activated"
+
 exit 0
