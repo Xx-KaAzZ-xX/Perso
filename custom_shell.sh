b2374aef4f62f8d9174e45053364633619da1db2
root <root@debian>
2016-11-06 15:26:31 +0100

    add color to root shell

--- custom_shell.sh
+++ custom_shell.sh
@@ -12,8 +12,10 @@ sed -i 's/^# alias l=/alias l=/g' /root/.bashrc
 echo "
 alias al=\"ls \$LS_OPTIONS -alh\" 
 alias showconnections=\"netstat -ntu | awk '{print \$5}' | cut -d: -f1 | grep -E [0-9.]+ | sort | uniq -c | sort -n\" 
+force_color_prompt=\"yes\"
+PS1='${debian_chroot:+($debian_chroot)}\[\033[01;95m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 " >> /root/.bashrc
 
-echo "Think to do a '. /root/.bashrc' if the modif aren't activated"
+echo "Think do : source /root/.bashrc"
 
 exit 0
