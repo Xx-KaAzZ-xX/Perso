06619cb9cd6c6d341fef5d9ed5798230cfe04770
root <root@localhost.localdomain>
2017-02-06 20:28:42 +0100

    check package installation

--- Python/info_gathering.py
+++ Python/info_gathering.py
@@ -1,10 +1,17 @@
 #!/bin/python
+# -*- coding: ISO-8859-1 -*- 
 
 #. Description:     Tool to automatize information gathering step
 
-import sys, os.path, apt
+import os, sys, os.path, apt, socket
+
+##Définitions des variables
 scriptname = sys.argv[0]
 cache = apt.Cache()
+prerequisites = "/tmp/prerequisites.txt"
+yes = set(['yes','y', 'ye', ''])
+no = set(['no','n'])
+
 
 ##Usage function
 def usage ():
@@ -15,15 +22,39 @@ def usage ():
 
 ##Check if necessary packages are installed
 def check_package():
-    if cache['nikto'].is_installed:
-        print "it works"
+    ##On définit une liste des paquets requis pour le script
+    file = open(prerequisites, 'wb')
+    file.write("nikto\ntor\nproxychains\nnmap")
+    file.close()
+    
+    ##On check si chacun d'eux est installé
+    with open(prerequisites, 'r') as f:
+        for line in f:
+            line = line.rstrip('\n')
+            if cache[line].is_installed: 
+                print (line+" :[OK]")
+            else:
+                print (line+" is not installed. Do you want the script to install it for you ? [Y/n]")
+                choice = raw_input().lower()
+                if choice in yes:
+                    os.system('apt-get install -y '+line)
+                else:
+                    print ("The script won't work without the "+line+"package")
+def scan_target():
+    target = sys.argv[2]
+    target_ip = socket.gethostbyname(target)
 
+
+def main_prog():
+    for arg in sys.argv:
+        if arg == "-h":
+            scan_target()
+        if len(sys.argv) == 1:
+            usage()
+
+print ("===== Prerequisites =====\n")
 check_package()
+print ("==========================\n")
+main_prog()
 
-'''
-for arg in sys.argv:
-    if arg == "-h":
-        print "It works"
-    if len(sys.argv) == 1:
-        usage()
-'''
+
