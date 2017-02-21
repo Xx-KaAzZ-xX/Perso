fbf644707fb03af91068025f88bc8e54e0b65c5a
root <root@localhost.localdomain>
2017-02-21 14:34:41 -0500

    fini l'output des URLs pour web.nvd.nist.gov

--- Python/scraper.py
+++ Python/scraper.py
@@ -1,11 +1,11 @@
-#/usr/bin/python
-
+#!/usr/bin/python
+# -*- coding: ISO-8859-1 -*-
 #. Description: Outil qui fait de la recherche dans plusieurs BDD CVE en fonction du -s search_term
 
-#.Pense-bête: 
+#. Pense-bête: 
 #. Où en étais-je: Améliorer l'output pour les CVE
 
-import sys, getopt, requests, re
+import os, sys, requests, re, subprocess
 
 ##Une fois que ça marche avec un site , il faut mettre la liste d'url avec params à modifier dans cette boucle
 ## WARNING : Les sites ne vont pas tous retournés les datas sous le même genre !
@@ -27,16 +27,26 @@ for arg in sys.argv:
         #print (search_term)
         cve1 = "cve1.html"
         cve2 = open(cve1, 'wb')
-        page = requests.get("https://web.nvd.nist.gov/view/vuln/search-results?query="+search_term)
+        base_url = "https://web.nvd.nist.gov/view/vuln/"
+        page = requests.get(base_url+"search-results?query="+search_term)
         content = page.content
         cve2.write(content)
-
         with open(cve1, 'r') as f:
             for line in f:
-                if re.search('CVE',line):
-                    print (line)
-
+                cve = re.search('CVE', line)
+                #start = "v2 - "
+                #end = ">CVE-"
+                if cve:
+                    ##C'est ici qu'il faut faire la regex en python pour faire ressortir la liste des URL
+                    #print((line.split(start))[0].split(end)[0])
+                    url = re.findall('detail(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line)
+                    for i in range(len(url)):
+                        print (base_url+url[i])
         cve2.close
+        #cve_id = os.system('sed -n \'s/.*href="\\([^"]*\\).*/\\1/p\' cve1.html | grep vulnId')
+        #print (cve_id)
+
+    
     if len(sys.argv) == 1:
         usage()
 
