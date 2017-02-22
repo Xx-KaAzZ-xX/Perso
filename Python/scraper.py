595755f83800f676361909f5355a4dfeaadbf4dd
root <root@mailer.adubus.com>
2017-02-22 17:06:30 +0100

    fini packetStormSecurity

--- Python/scraper.py
+++ Python/scraper.py
@@ -9,12 +9,6 @@ import os, sys, requests, re, subprocess
 
 ##Une fois que ça marche avec un site , il faut mettre la liste d'url avec params à modifier dans cette boucle
 ## WARNING : Les sites ne vont pas tous retournés les datas sous le même genre !
-'''
-with open('sites.txt', 'r')as f:
-    for line in f:
-        page = requests.get(line+'apache2.4')
-        content = page.content
-'''
 
 class bcolors:
     HEADER = '\033[95m'
@@ -31,12 +25,12 @@ for arg in sys.argv:
         search_term = sys.argv[2]
 
     ##NVD DATABASE PART##
-        '''
+        
         print (bcolors.OKGREEN+"\t \t \t ====== NVD DATABASE ======")
         cve1 = "cve1.html"
         cve2 = open(cve1, 'wb')
-        base_url = "https://web.nvd.nist.gov/view/vuln/"
-        page = requests.get(base_url+"search-results?query="+search_term)
+        base_url1 = "https://web.nvd.nist.gov/view/vuln/"
+        page = requests.get(base_url1+"search-results?query="+search_term)
         content = page.content
         cve2.write(content)
         with open(cve1, 'r') as f:
@@ -45,34 +39,46 @@ for arg in sys.argv:
                 if cve:
                     ##C'est ici qu'il faut faire la regex en python pour faire ressortir la liste des URL
                     #print((line.split(start))[0].split(end)[0])
-                    url = re.findall('detail(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line)
-                    for i in range(len(url)):
-                        print (base_url+url[i])
+                    url1 = re.findall('detail(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line)
+                    for i in range(len(url1)):
+                        print (base_url1+url1[i])
         cve2.close
-        '''
-        ##0DAY DATABASE PART##
+        print (bcolors.OKGREEN+"========================================")       
+        ##PacketStorm Security DATABASE PART##
         
-        print (bcolors.OKBLUE+"\t \t \t ====== ODAY DATABASE ======")
-        cve1 = "cve1.html"
-        cve2 = open(cve1, 'wb')
-        #base_url = "https://web.nvd.nist.gov/view/vuln/"
-        page = requests.get("http://0day.today/search?search_request="+search_term)
+        print (bcolors.OKBLUE+"\t \t \t ====== PacketStormSecurity DATABASE ======")
+	cve3 = "cve2.html"
+        cve4 = open(cve3, 'wb')
+        base_url2 = "https://packetstormsecurity.com"
+        page = requests.get(base_url2+"/search/?q="+search_term)
         content = page.content
-        
-        cve2.write(content)
-        with open(cve1, 'r') as f:
+        cve4.write(content)
+        sys.stdout = open("cve_packet.txt", 'w')
+        with open(cve3, 'r') as f:
             for line in f:
+
                 cve = re.search('CVE', line)
-                #start = "v2 - "
-                #end = ">CVE-"
                 if cve:
                     ##C'est ici qu'il faut faire la regex en python pour faire ressortir la liste des URL
-                    #print((line.split(start))[0].split(end)[0])
-                    url = re.findall('detail(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line)
-                    for i in range(len(url)):
-                        print (url[i])
-        cve2.close
-        
+                    url2 = re.findall('/files(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line)
+
+                    for i in range(len(url2)):
+                        print (base_url2+url2[i])
+                        #cve_packet.write(test)
+                        #cve_packet.close
+                        #test2 = re.findall('/files/cve/CVE-(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line)
+
+
+        sys.stdout.close
+        cve4.close
+
+        sys.stdout = open("/dev/stdout", "w")
+        os.system('cat cve_packet.txt | sort -u > cve_packet2.txt')
+        with open("cve_packet2.txt", "r") as f:
+            for line in f:
+                print (line)
+        #os.system('rm cve_packet.txt')                
+        print (bcolors.OKBLUE+"========================================")       
 
 if len(sys.argv) == 1:
         usage()
