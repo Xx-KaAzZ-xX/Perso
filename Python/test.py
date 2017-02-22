986547cbd13b04e30b3adcb0b9ad201f88a012b0
Xx-KaAzZ-xX <Xx-KaAzZ-xX@users.noreply.github.com>
2017-02-22 15:04:36 +0100

    Update test.py

--- Python/test.py
+++ Python/test.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
 
-import requests, sys, re
+import re, requests, sys
 
 for arg in sys.argv:
         if arg == "-s":
             search_term = sys.argv[2]
+            sys.stdout = open("cve_packet.txt", 'w')
             cve3 = "cve2.html"
             cve4 = open(cve3, 'wb')
             base_url = "https://packetstormsecurity.com"
@@ -14,14 +15,21 @@ for arg in sys.argv:
             cve4.write(content)
             with open(cve3, 'r') as f:
                 for line in f:
+                    
                     cve = re.search('CVE', line)
                     if cve:
-                        print(line)
+                        #print(line)
                         
                         ##C'est ici qu'il faut faire la regex en python pour faire ressortir la liste des URL
-                        url = re.findall('/files(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line)
+                        url = re.findall('/files(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line)                   
+                        
                         for i in range(len(url)):
-                            test = print (base_url+url[i])
-                            test2 = re.findall('https://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', test)
-                            print (test2)
+                            
+                            print (base_url+url[i])
+                            
+                            #cve_packet.write(test)
+                            #cve_packet.close
+                            #test2 = re.findall('/files/cve/CVE-(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line)
+                        
+            sys.stdout.close
             cve4.close
