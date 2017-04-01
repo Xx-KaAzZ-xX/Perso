541c9b4757d9efd764fc13cb6fe9dd19600edac7
Xx-KaAzZ-xX <dublatub01@gmail.com>
2017-04-01 17:20:13 -0400

    to test with other CMS than wordpress

--- Bash/checkWebDir.sh
+++ Bash/checkWebDir.sh
@@ -10,19 +10,60 @@ usage() {
   echo -e "\t  -d : Specify the directory to watch."
   echo -e "\t  -h : Displays this help."
   echo -e "\n"
-  echo -e "\n"
   echo -e "Exemple : ./${script_name} -d /home/user/www"
 }
 
-if [ -z "$" ] || [ -z "$" ]; then
-    usage
+if [ -z "${1}" ]; then
+  usage
 fi
 
 while getopts ":d:h:" opt; do
     case "$opt" in
         d)  dir="$OPTARG"
-            php_files=$(find $dir -type f -name *.php)
-            echo "$php_files"
+            tmp1="/tmp/php_list_1.txt"
+            tmp2="/tmp/php_list_2.txt"
+            tmp3="/tmp/checksum_list1.txt"
+            tmp4="/tmp/checksum_list2.txt"
+            
+            ##Before modifications
+
+            php_files_before=$(find $dir -type f -name *.php)
+            echo "$php_files_before" > $tmp1
+            if [ -f "${tmp3}" ]; then
+              rm $tmp3
+            fi
+            cat $tmp1 | while read line; do
+                md5sum $line >> $tmp3
+              done
+            
+            ##After modifications
+            
+            while true; do
+              php_files_after=$(find $dir -type f -name *.php)
+              #sleep 15
+              echo "$php_files_after" > $tmp2
+              
+              #echo " " >> $dir/wp-settings.php
+              
+              if [ -f "${tmp4}" ]; then
+                rm $tmp4
+              fi
+
+              cat $tmp2 | while read line; do
+                md5sum $line >> $tmp4
+              done
+                
+              modified_file=$(diff $tmp3 $tmp4 | awk '{print $3}' | sort -u)
+              
+              if [ -z "${modified_file}" ]
+              then
+                :
+              else
+                ##Alert
+                echo "$modified_file modifié(s) !"
+              fi
+              sleep 15
+            done
             ;;
         h)
             usage
@@ -34,6 +75,10 @@ while getopts ":d:h:" opt; do
 done
 shift $((OPTIND-1))
 
+
+if [ -z "$" ] || [ -z "$" ]; then
+    usage
+fi
 
 
 
