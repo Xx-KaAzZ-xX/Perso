9b156b4b8d3a473a84573fb740bd5ddd7601537f
root <root@debian>
2017-04-27 17:13:58 +0200

    reste des choses à modifer

--- Bash/Web/nginx_with_modsecurity.sh
+++ Bash/Web/nginx_with_modsecurity.sh
@@ -36,118 +36,116 @@ tar -xvf nginx-1.6.2.tar.gz > /dev/null
 make 
 cd /root/nginx-1.6.2
 
-./configure --conf-path=/etc/nginx/conf/nginx.conf --http-log-path=/var/log/nginx/access.log --error-log-path=/var/log/nginx/error.log --with-http_ssl_module --add-module=/root/modsecurity/nginx/modsecurity/
+#./configure --conf-path=/etc/nginx/conf/nginx.conf --http-log-path=/var/log/nginx/access.log --error-log-path=/var/log/nginx/error.log --with-http_ssl_module --add-module=/root/modsecurity/nginx/modsecurity/
+./configure --conf-path=/etc/nginx/nginx.conf --add-module=/root/modsecurity/nginx/modsecurity/  --error-log-path=/var/log/nginx/error.log --http-client-body-temp-path=/var/lib/nginx/body  --http-fastcgi-temp-path=/var/lib/nginx/fastcgi --http-log-path=/var/log/nginx/access.log  --http-proxy-temp-path=/var/lib/nginx/proxy --lock-path=/var/lock/nginx.lock  --pid-path=/var/run/nginx.pid --with-http_ssl_module  --without-mail_pop3_module --without-mail_smtp_module  --without-mail_imap_module --without-http_uwsgi_module  --without-http_scgi_module --with-ipv6 --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx
 echo -e "\n Compiling Nginx... \n"
 
-#echo "dire où les fichiers iront si on laisse par défaut , ou mettre des variables pour la compil"
 make
 make install
-#Symbolic link to access the nginx command from anywhere
-ln -s /usr/local/nginx/sbin/nginx /usr/sbin/nginx
+
+cat > /etc/nginx/nginx.conf << _EOF_
+user www-data;
+worker_processes 4;
+pid /run/nginx.pid;
+
+events {
+        worker_connections 768;
+        # multi_accept on;
+}
+
+http {
+
+        ##
+        # Basic Settings
+        ##
+
+        sendfile on;
+        tcp_nopush on;
+        tcp_nodelay on;
+        keepalive_timeout 65;
+        types_hash_max_size 2048;
+        # server_tokens off;
+
+        # server_names_hash_bucket_size 64;
+        # server_name_in_redirect off;
+
+        include /etc/nginx/mime.types;
+        include naxsi_core.rules;
+        default_type application/octet-stream;
+        client_max_body_size 20M;
+        ##
+        # SSL Settings
+        ##
+
+        ssl_protocols TLSv1 TLSv1.1 TLSv1.2; # Dropping SSLv3, ref: POODLE
+        ssl_prefer_server_ciphers on;
+
+        ##
+        # Logging Settings
+        ##
+
+        access_log /var/log/nginx/access.log;
+        error_log /var/log/nginx/error.log;
+
+        ##
+        # Gzip Settings
+        ##
+
+        gzip on;
+        gzip_disable "msie6";
+
+        # gzip_vary on;
+        # gzip_proxied any;
+        # gzip_comp_level 6;
+        # gzip_buffers 16 8k;
+        # gzip_http_version 1.1;
+        # gzip_types text/plain text/css application/json application/javascript text/xml application/xml application/xml+rss text/javascript;
+
+        ##
+        # Virtual Host Configs
+        ##
+
+        include /etc/nginx/conf.d/*.conf;
+        include /etc/nginx/sites-enabled/*;
+}
+_EOF_
+
+mkdir /etc/nginx/sites-available /etc/nginx/sites-enabled /etc/nginx/conf.d
 
 #Creating files to manage the nginx service
 echo -e "\n Creating nginx.service file...\n"
 touch /lib/systemd/system/nginx.service
-cat >> /lib/systemd/system/nginx.service << _EOF_
+cat > /lib/systemd/system/nginx.service << _EOF_
+[Unit]
+Description=The NGINX HTTP and reverse proxy server
+After=syslog.target network.target remote-fs.target nss-lookup.target
+
 [Service]
 Type=forking
-ExecStartPre=/usr/local/nginx/sbin/nginx -t -c /etc/nginx/conf/nginx.conf
-ExecStart=/usr/local/nginx/sbin/nginx -c /etc/nginx/conf/nginx.conf
-ExecReload=/usr/local/nginx/sbin/nginx -s reload
-KillStop=/usr/local/nginx/sbin/nginx -s stop
- 
-KillMode=process
-Restart=on-failure
-RestartSec=42s
- 
+PIDFile=/run/nginx.pid
+ExecStartPre=/usr/sbin/nginx -t
+ExecStart=/usr/sbin/nginx
+ExecReload=/bin/kill -s HUP $MAINPID
+ExecStop=/bin/kill -s QUIT $MAINPID
 PrivateTmp=true
-LimitNOFILE=200000
- 
+
 [Install]
 WantedBy=multi-user.target
 _EOF_
 systemctl daemon-reload
 
-echo -e "\n Creating /etc/init.d/nginx file...\n"
-touch /etc/init.d/nginx
-cat >> /etc/init.d/nginx << _EOF_
-#!/bin/bash
- 
-### BEGIN INIT INFO
-# Provides:          nginx
-# Required-Start:    $all
-# Required-Stop:     $all
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: starts the nginx web server
-# Description:       starts nginx using start-stop-daemon
-### END INIT INFO
- 
-PATH=/opt/bin:/opt/sbin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-DAEMON=/usr/local/nginx/sbin/nginx
-NAME=nginx
-DESC=nginx
- 
-test -x $DAEMON || exit 0
- 
-# Include nginx defaults if available
-if [ -f /etc/default/nginx ] ; then
-        . /etc/default/nginx
-fi
- 
-set -e
- 
-case "$1" in
-  start)
-        echo -n "Starting $DESC: "
-        start-stop-daemon --start --quiet --pidfile /var/run/nginx.pid \
-                --exec $DAEMON -- $DAEMON_OPTS
-        echo "$NAME."
-        ;;
-  stop)
-        echo -n "Stopping $DESC: "
-        start-stop-daemon --stop --quiet --pidfile /var/run/nginx.pid \
-                --exec $DAEMON
-        echo "$NAME."
-        ;;
-  restart|force-reload)
-        echo -n "Restarting $DESC: "
-        start-stop-daemon --stop --quiet --pidfile \
-                /var/run/nginx.pid --exec $DAEMON
-        sleep 1
-        start-stop-daemon --start --quiet --pidfile \
-                /var/run/nginx.pid --exec $DAEMON -- $DAEMON_OPTS
-        echo "$NAME."
-        ;;
-  reload)
-      echo -n "Reloading $DESC configuration: "
-      start-stop-daemon --stop --signal HUP --quiet --pidfile /var/run/nginx.pid \
-          --exec $DAEMON
-      echo "$NAME."
-      ;;
-  *)
-        N=/etc/init.d/$NAME
-        echo "Usage: $N {start|stop|restart|force-reload}" >&2
-        exit 1
-        ;;
-esac
- 
-exit 0
-_EOF_
-chmod +x /etc/init.d/nginx
-
 #Configuration of modsecurity
-cp /root/modsecurity/modsecurity.conf-recommended /etc/nginx/conf/modsecurity.conf
-cp /root/modsecurity/unicode.mapping /etc/nginx/conf/ 
+cp /root/modsecurity/modsecurity.conf-recommended /etc/nginx/conf.d/modsecurity.conf
+cp /root/modsecurity/unicode.mapping /etc/nginx/conf.d/ 
 
 #Adding OWASP Rules
 cd /usr/src/
 git clone https://github.com/SpiderLabs/owasp-modsecurity-crs.git
 cd owasp-modsecurity-crs
-cp -R base_rules/ /etc/nginx/conf/
+cp -R base_rules/ /etc/nginx/conf.d/
 
 #Editing modsecurity.conf
-cat >> /etc/nginx/conf/modsecurity.conf << _EOF_
+cat >> /etc/nginx/conf.d/modsecurity.conf << _EOF_
 #DefaultAction
 SecDefaultAction "log,deny,phase:1"
 
@@ -160,16 +158,13 @@ _EOF_
 
 #Editing nginx.conf
 
-if [ -f /etc/nginx/conf/modsecurity.conf ]; then
-  sed -i 's#^SecRuleEngine\(.*\)$#SecRuleEngine On#g' /etc/nginx/conf/modsecurity.conf
+if [ -f /etc/nginx/conf.d/modsecurity.conf ]; then
+  sed -i 's#^SecRuleEngine\(.*\)$#SecRuleEngine On#g' /etc/nginx/conf.d/modsecurity.conf
   # Fix upload max file size at 32M
-  sed -i 's#^SecRequestBodyLimit\(.*\)$#SecRequestBodyLimit 32768000#g' /etc/nginx/conf/modsecurity.conf
-  sed -i 's#^SecRequestBodyInMemoryLimit\(.*\)$#SecRequestBodyInMemoryLimit 32768000#g' /etc/nginx/conf/modsecurity.conf
-  sed -i 's#^SecResponseBodyAccess\(.*\)$#SecResponseBodyAccess Off#g' /etc/nginx/conf/modsecurity.conf
+  sed -i 's#^SecRequestBodyLimit\(.*\)$#SecRequestBodyLimit 32768000#g' /etc/nginx/conf.d/modsecurity.conf
+  sed -i 's#^SecRequestBodyInMemoryLimit\(.*\)$#SecRequestBodyInMemoryLimit 32768000#g' /etc/nginx/conf.d/modsecurity.conf
+  sed -i 's#^SecResponseBodyAccess\(.*\)$#SecResponseBodyAccess Off#g' /etc/nginx/conf.d/modsecurity.conf
 fi
-
-#Activation du module ModSecurity
-sed -i '/gzip/a \#Enable ModSecurity\nModSecurityEnabled on;\nModSecurityConfig modsecurity.conf;\n' /etc/nginx/conf/nginx.conf
 
 echo -e "\n Start Nginx Server\n"
 systemctl start nginx
