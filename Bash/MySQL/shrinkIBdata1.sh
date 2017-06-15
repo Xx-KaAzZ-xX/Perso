2011fbac90739239b487146109925bfadccad33c
root <root@debian>
2017-06-15 12:33:16 +0200

    à tester

--- Bash/MySQL/shrinkIBdata1.sh
+++ Bash/MySQL/shrinkIBdata1.sh
@@ -10,6 +10,7 @@ DBLIST="ListOfDatabases.txt"
 DBHOST="localhost"
 DBUSER="root"
 DBPASS="root"
+CustomSQL="/etc/mysql/conf.d/customIT.cnf"
 
 ##Step one : backup all DBs into a file
 
@@ -21,8 +22,36 @@ mysql -h ${DBHOST} -u ${DBUSER} -p""${DBPASS}"" -A --skip-column-names -e"SELECT
 
 for DB in `cat ${DBLIST}`
 do
-
+SQL_QUERY="DROP DATABASE ${DB};"
+mysql -u root -p""${DBPASS}"" -e "${SQL}"
 done
 
+##Step three : Shutdown MySQL
+
+systemctl stop mysql
+
+##Step four : custom conf
+if [ -f ${CustomSQL} ]; then
+  echo "innodb_file_per_table" >> ${CustomSQL}
+else
+  cat >> ${CustomSQL} << _EOF_
+[mysqld]
+innodb_file_per_table
+innodb_flush_method=O_DIRECT
+_EOF_
+
+##Step 05 : Delete ibdata1, ib_logfile0 and ib_logfile1
+
+rm /var/lib/mysql/ibdata1 /var/lib/mysql/ib_logfile0 /var/lib/mysql/ib_logfile1
+
+##Step 06 : Restart MySQL
+
+systemctl restart mysql
+
+##Step 07 : Reload MySQL data into MySQL
+
+mysql -h ${DBHOST} -u ${DBUSER} -p""${DBPASS}"" < ${FILE}
+
+echo "With the innodb_file_per_table option, you can now run OPTIMIZE TABLE mydb.mytable on each DB to shrink the mytable.ibd file"
 
 exit 0
