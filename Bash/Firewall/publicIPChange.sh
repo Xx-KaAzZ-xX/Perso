d5bd2e0728092ee741ea4386c48baa785403b47f
root <root@localhost.localdomain>
2017-08-27 15:20:27 +0200

    to test

--- Bash/Firewall/publicIPChange.sh
+++ Bash/Firewall/publicIPChange.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
 
-ip1="/tmp/ip1.txt"
-ip2="/tmp/ip2.txt"
-curl ifconfig.me 2>/dev/null > $ip1
-
-if [ -f $ip2 ]
-then
-  rm $ip2
-fi
-
 while true
 do
+
+  ip1="/tmp/ip1.txt"
+  ip2="/tmp/ip2.txt"
+  curl -s ifconfig.me  > $ip1
   before=$(cat $ip1)
-  sleep 5
-  curl ifconfig.me 2>/dev/null > $ip2
+  sleep -m 60
+  curl -s ifconfig.me > $ip2
   after=$(cat $ip2)
 
   if [[ "${before}" != "${after}" ]]
   then
-    echo ${after}
-    #echo "Nouvelle IP de connexion : ${after}" | mail -s "New Public IP"  al3rte5@gmail.com
-  else
-    echo "same"
+    echo "Nouvelle IP de connexion : ${after}" | mail -s "New Public IP"  al3rte5@gmail.com
   fi
+
 done
 
 exit 0
