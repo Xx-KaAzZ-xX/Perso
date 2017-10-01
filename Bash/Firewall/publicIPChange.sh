52c7df4dc13c4619f8962de2973013d8458b6898
root <root@raspberrypi.lan>
2017-10-01 08:46:46 +0000

    best way to get publicIP

--- Bash/Firewall/publicIPChange.sh
+++ Bash/Firewall/publicIPChange.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
-while true
-do
 
-  ip1="/tmp/ip1.txt"
-  ip2="/tmp/ip2.txt"
-  curl -s ifconfig.me  > $ip1
-  before=$(cat $ip1)
-  sleep -m 60
-  curl -s ifconfig.me > $ip2
-  after=$(cat $ip2)
-
-  if [[ "${before}" != "${after}" ]]
-  then
-    echo "Nouvelle IP de connexion : ${after}" | mail -s "New Public IP"  al3rte5@gmail.com
-  fi
-
-done
+before=$(dig +short myip.opendns.com @resolver1.opendns.com)
+sleep 60m
+after=$(dig +short myip.opendns.com @resolver1.opendns.com)
+if [[ -z "${after}" ]];then
+  exit 1
+elif [[ "${before}" != "${after}" ]]
+then
+  echo "Nouvelle IP de connexion : ${after}" | mail -s "New Public IP"  al3rte5@gmail.com
+fi
 
 exit 0
