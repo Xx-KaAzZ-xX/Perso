26fb2ac43cf071f7a64c173bc18528116bc53eb7
root <root@desktop>
2018-11-17 16:45:52 +0100

    maj

--- Python/decode_cesarASCII.py
+++ Python/decode_cesarASCII.py
@@ -1,21 +1,48 @@
 #!/usr/bin/python
-#encoding: utf8
+# encoding: utf8
 
+## Script qui permet de changer chaque caractere du fichier en code ASCII, de bruteforce
+## le codage cesar donc de 0 à 128 mais avec + et - et de retransformer le code
+## ASCII du caractère chiffré en caractère lisible
 file = "ch7.bin"
-LETTERS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+array = []
 with open(file, 'r') as content_file:
     content = content_file.read()
-    print (content)
-    # loop through every possible key
-for key in range(len(LETTERS)):
-	translated = ''
-	for symbol in content:
-		if symbol in LETTERS:
-			num = LETTERS.find(symbol) # get the number of the symbol
-			num = num - key
-		if num < 0:
-			num = num + len(LETTERS)
-			translated = translated + LETTERS[num]
-		else:
-			translated = translated + symbol
-	print('Key #%s: %s' % (key, translated))
+    #print (content)
+    for rank in range(len(content)):
+    	char = content[rank]
+    	array += [ord(char)]
+    #print (array)
+
+    for i in range(128):
+    	string = "KEY #"
+    	string += str(i)
+    	print string
+    	decipher_array = []
+    	decipher_array2 = []
+    	for x in array:
+    		#print (x)
+    		new_ascii_code = x + i	
+    		decipher_array += [new_ascii_code]
+    		new_ascii_code2 = x - i
+    		decipher_array2 += [new_ascii_code2]
+    	#print(decipher_array)
+    	for y in decipher_array:
+    		try:
+    			test = chr(y)
+    			print (test),
+    			#break
+    		except ValueError:
+    			print "Pas de code correspondant au caractère"
+    	print ("ANOTHER POSSIBLE KEY ")
+    	for y in decipher_array2:
+    		try:
+    			test2 = chr(y)
+    			print (test2),
+    			#break
+    		except ValueError:
+    			print "Pas de code correspondant au caractère"
+    	#print (test)
+    	del decipher_array[:]
+    	del decipher_array2[:]
+    		
