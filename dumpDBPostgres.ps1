d8767d9e81af592ff808bc6b88e2ce3db07aca09
Xx-KaAzZ-xX <Xx-KaAzZ-xX@users.noreply.github.com>
2019-01-24 20:57:03 +0100

    Delete dumpDBPostgres.ps1

--- dumpDBPostgres.ps1
+++
@@ -1,9 +0,0 @@
-$databases = ("test1", "test2", "test3")
-$user = "postgres"
-$bkp_dir = "O:\DB\"
-$date = Get-Date -Format yyyyMMdd
-
-foreach ($db in $databases){
-    $filename = "$bkp_dir$date$db.sql"
-    C:\PostgreSQL\pg96\bin\pg_dump.exe -U $user -f "$filename" $db
-}
