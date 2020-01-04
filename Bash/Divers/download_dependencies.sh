13033d2e10dacfcfb0aa6edb362348e859b3ba5d
root <root@desktop>
2020-01-04 15:28:54 +0100

    add download_dependencies.sh to repo

---
+++ Bash/Divers/download_dependencies.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#.  Author : Aurélien DUBUS
+#.  Version : 1.0
+#.  Description : Script that allows to download a package and all it's dependencies.
+
+usage() {
+ script_name=$(basename $0)
+   echo -e "Usage : ./${script_name} <package name>"
+   exit 1
+}
+
+if [ -z "${1}" ]
+then
+  usage
+fi
+
+dir="${1}_deb_files"
+mkdir "${dir}"
+echo "Downloading  the dependencies..."
+cd "${dir}"
+apt-get download ${1} && apt-cache depends -i ${1} | awk '/Dépend:/ {print $2}' | xargs apt-get download
+
+echo "All deb files are in ${dir}"
+
+exit 0
+
+
