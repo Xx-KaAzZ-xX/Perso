f66b90ef9da43adef8f7b4c74665ec9b80b723e4
you@example.com <you@example.com>
2020-07-21 20:45:35 +0200

    à tester

--- Bash/Security/OS_hardening.sh
+++ Bash/Security/OS_hardening.sh
@@ -53,3 +53,30 @@ _EOF_
 
 mv /etc/sysctl.conf /etc/systcl.bak
 mv ./${SYSCTL_FILE} /etc/sysctl.conf
+
+##Configuration de la longueur minimale de mots de passe, de pam_tally2 pour bloquer un compte lors des échecs, 
+cp /etc/pam.d/passwd /etc/pam.d/passwd.bak
+cp /etc/pam.d/login /etc/pam.d/login.bak
+cp /etc/pam.d/common-password /etc/pam.d/common-password.bak
+cp /etc/login.defs /etc/login.defs.bak
+
+echo -e "#Fichier/etc/pam.d/passwd
+#Aumoins12caractères,pasderépétitionnideséquencemonotone,
+#3classesdifférentes(parmimajuscules,minuscules,chiffres,autres)
+passwordrequired	pam_cracklib.so minlen=12 minclass=3	\
+				dcredit=0 ucredit=0 lcredit=0	\
+				ocredit=0 maxrepeat=1		\
+				maxsequence=1 gecoscheck	\
+				reject_username enforce_for_root" | tee -a /etc/pam.d/passwd
+
+echo -e "# Blocage du compte pendant 5 min après 3 échecs
+auth	required	pam_tally.so	deny=3	lock_time=300" | tee -a /etc/pam.d/login
+
+##Durcissement du stockage des mots de passe
+echo -e "password	required	pam_unix.so	obscure sha512 rounds=65536" | tee -a /etc/pam.d/common-password
+echo -e "ENCRYPT_METHOD	SHA512
+SHA_CRYPT_MIN_ROUNDS	65536" | tee -a /etc/login.defs
+sed '/UMASK/d' /etc/login.defs
+echo "UMASK	027" | tee -a /etc/login.defs
+
+exit 0
