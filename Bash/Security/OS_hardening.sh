3721481935b26edd0953978dadc34bea1ba340fd
root <dublatub01@gmail.com>
2021-05-10 22:04:07 +0200

    little maj

--- Bash/Security/OS_hardening.sh
+++ Bash/Security/OS_hardening.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-##Variables
+##Variables à définir avant l'exécution du script
 DOMAIN_NAME="ad.lan"
+ADMIN_GROUP="Administrateurs"
 SYSCTL_FILE="anssi_sysctl-conf"
 
 #Check if machine is in Active Directory
-
 realm list | grep active-directory > /dev/null 2>&1
-#[ $? -eq 0 ]  || echo "Machine not in AD, the script will exit" && exit 1
 
+if [[ $? -eq 0 ]] ; then
 ##Si la machine est dans le domaine, on autorise les admins à se connecter
-realm permit -g Administrateurs > /dev/null 2>&1
+realm permit -g ${ADMIN_GROUP} > /dev/null 2>&1
 
-##On autorise ensuite les membres du groupe d'admin à lancer sudo
-mv /etc/sudoers /etc/sudoers.bak
-cat >> /etc/sudoers << _EOF_
+	##On autorise ensuite les membres du groupe d'admin à lancer sudo
+	mv /etc/sudoers /etc/sudoers.bak
+	cat >> /etc/sudoers << _EOF_
 ## Sudoers allows particular users to run various commands as
 ## the root user, without needing the root password.
 ##
@@ -43,7 +43,7 @@ Defaults    secure_path = /sbin:/bin:/usr/sbin:/usr/bin
 root    ALL=(ALL)   ALL
 
 #Autorisation des admins à lancer la commande sudo
-%Administrateurs@${DOMAIN_NAME}     ALL=(ALL)   ALL
+%${ADMIN_GROUP}@${DOMAIN_NAME}     ALL=(ALL)   ALL
 _EOF_
 
 
@@ -68,9 +68,11 @@ passwordrequired	pam_cracklib.so minlen=12 minclass=3	\
 				ocredit=0 maxrepeat=1		\
 				maxsequence=1 gecoscheck	\
 				reject_username enforce_for_root" | tee -a /etc/pam.d/passwd
-
+echo "[-] Password minimal lenght : OK"
 echo -e "# Blocage du compte pendant 5 min après 3 échecs
 auth	required	pam_tally.so	deny=3	lock_time=300" | tee -a /etc/pam.d/login
+
+echo "[-] Account locked after 3 fails : OK"
 
 ##Durcissement du stockage des mots de passe
 echo -e "password	required	pam_unix.so	obscure sha512 rounds=65536" | tee -a /etc/pam.d/common-password
