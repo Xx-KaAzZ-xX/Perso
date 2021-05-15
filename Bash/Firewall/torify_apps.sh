7e97f1721e08f588b6be23397665747bf6378a19
root <dublatub01@gmail.com>
2021-05-15 22:04:10 +0200

    maj torify apps

--- Bash/Firewall/torify_apps.sh
+++ Bash/Firewall/torify_apps.sh
@@ -12,7 +12,7 @@ echo 'nameserver 127.0.0.1' > /etc/resolv.conf
 ### set variables
 #_tor_uid=$(/etc/init.d/tor status | grep Main | awk '{print $3}')
 _tor_uid=$(ps -aux | grep /usr/bin/tor | sed -n 1p | awk '{printf $2}')
-_out_if="eth0"
+_out_if="wlp5s0"
 _trans_port="9040"
 _dns_port="5353"
 _non_tor="127.0.0.0/8 10.0.0.0/8 172.16.0.0/12 192.168.0.0/16"
@@ -22,13 +22,11 @@ _virt_addr="10.192.0.0/10"
 username="${1}"
 
 
-#iptables -A INPUT -p tcp -m tcp --dport 22 -s 192.168.1.0/24 -j ACCEPT
-#iptables -A OUTPUT -p tcp -m tcp --dport 22 -s 192.168.1.0/24 -j ACCEPT
-iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner ${username} -m tcp --syn -j REDIRECT --to-ports 9040
-iptables -t nat -A OUTPUT -p udp -m owner --uid-owner ${username} -m udp --dport 53 -j REDIRECT --to-ports 5353
-iptables -t filter -A OUTPUT -p tcp -m owner --uid-owner ${username} -m tcp --dport 9040 -j ACCEPT
-iptables -t filter -A OUTPUT -p udp -m owner --uid-owner ${username} -m udp --dport 5353 -j ACCEPT
-#iptables -t filter -A OUTPUT ! -o lo -m owner --uid-owner ${username} -j DROP
+iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner ${username} -m tcp --syn -j REDIRECT --to-ports ${_trans_port}
+iptables -t nat -A OUTPUT -p udp -m owner --uid-owner ${username} -m udp --dport 53 -j REDIRECT --to-ports ${_dns_port}
+iptables -t filter -A OUTPUT -p tcp -m owner --uid-owner ${username} -m tcp --dport ${_trans_port} -j ACCEPT
+iptables -t filter -A OUTPUT -p udp -m owner --uid-owner ${username} -m udp --dport ${_dns_port} -j ACCEPT
+
 ##Test
 su - ${username} -c "nslookup google.fr" > /dev/null  2>&1
 test=$(echo $?)
